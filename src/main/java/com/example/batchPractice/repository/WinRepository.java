8113e6d22cde84892f628c07b8dcdc3134451109
hanachooi <jjina.js@gmail.com>
2024-09-20 16:52:12 +0900

    8강 조건에 따른 배치 처리 ( 컨트롤러로 job 실행함 )     하나의 테이블을 업데이트 하는 방식으로 코드가 짜여짐.     7강에서는 두개의 테이블을 이용하였음.

---
+++ src/main/java/com/example/batchPractice/repository/WinRepository.java
@@ -0,0 +1,12 @@
+package com.example.batchPractice.repository;
+
+import com.example.batchPractice.entity.WinEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinRepository extends JpaRepository<WinEntity, Long> {
+
+    Page<WinEntity> findByWinGreaterThanEqual(Long win, Pageable pageable);
+
+}
